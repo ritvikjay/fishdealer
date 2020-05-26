99d3a0e59d891b9e8aedbc0df97244153da276ab
Ritvik Jayakumar <ritvik.jay@gmail.com>
2020-05-26 17:22:38 -0400

    Update fishdealerbot.py
    Streamlined picking process to work for 4v4 and 3v3. Restructured players list based on teams in start_game section to make the two players with less cards in 4v4 split between teams.

--- fishdealerbot.py
+++ fishdealerbot.py
@@ -51,6 +51,7 @@ class FishBot(fbchat.Client):
     cardsdict_text[ind] = 'Black Joker'
 
 
+    captturn = -1
     players = []
     playernames = set()
     pickednames = set()
@@ -102,52 +103,79 @@ class FishBot(fbchat.Client):
                 self.send(Message(text=pickmsg), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
                 available = "Available: "+str(self.playernames-self.pickednames)
                 self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
+                self.captturn = 1
             elif('!pick' in message_object.text):
                 if(self.picking):
-                    if(author_id==self.team1[0].uid and len(self.team1)==1):
+                    if(author_id==self.team1[0].uid and self.captturn==1):
                         pickedname = message_object.text.replace('!pick','').strip()
-                        print(pickedname)
+                        # print(pickedname)
                         if(pickedname in self.playernames and pickedname not in self.pickednames):
                             self.pickednames.add(pickedname)
                             for player in self.players:
                                 if(player.name==pickedname):
                                     self.team1.append(player)
                                     break
-                            pickmsg = self.team2[0].name+" make two picks"
-                            self.sendMessage(pickmsg, thread_id = self.gcuid, thread_type = ThreadType.GROUP)
-                            available = "Available: "+str(self.playernames-self.pickednames)
-                            self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
+                            if((len(self.playernames)-len(self.pickednames))==1):
+                                for player in self.players:
+                                    if(player.name not in self.pickednames):
+                                        self.team2.append(player)
+                                self.teams = "Team 1: "+str([player.name for player in self.team1])+", Team 2: "+str([player.name for player in self.team2  ])
+                                self.send(Message(text=self.teams), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
+                                self.picking = False
+                                self.capturn = -1
+                            elif(len(self.team1)<=len(self.team2)):
+                                pickmsg = self.team1[0].name+" make a pick"
+                                self.sendMessage(pickmsg, thread_id = self.gcuid, thread_type = ThreadType.GROUP)
+                                available = "Available: "+str(self.playernames-self.pickednames)
+                                self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
+                            else:
+                                self.capturn = 2
+                                pickmsg = self.team2[0].name+" make a pick"
+                                self.sendMessage(pickmsg, thread_id = self.gcuid, thread_type = ThreadType.GROUP)
+                                available = "Available: "+str(self.playernames-self.pickednames)
+                                self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
                         else:
                             self.sendMessage("Invalid pick :|", thread_id = self.gcuid, thread_type = ThreadType.GROUP)
-                    elif(author_id==self.team2[0].uid and len(self.team1)==2):
+                    elif(author_id==self.team2[0].uid):
                         pickedname = message_object.text.replace('!pick','').strip()
-                        print(pickedname)
+                        # print(pickedname)
                         if(pickedname in self.playernames and pickedname not in self.pickednames):
                             self.pickednames.add(pickedname)
                             for player in self.players:
                                 if(player.name==pickedname):
                                     self.team2.append(player)
                                     break
-                            if(len(self.team2)==2):
-                                pickmsg = self.team2[0].name+" make one more pick"
-                                self.sendMessage(pickmsg, thread_id = self.gcuid, thread_type = ThreadType.GROUP)
-                                available = "Available: "+str(self.playernames-self.pickednames)
-                                self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
-                            else:
+                            if((len(self.playernames)-len(self.pickednames))==1):
                                 for player in self.players:
                                     if(player.name not in self.pickednames):
                                         self.team1.append(player)
                                 self.teams = "Team 1: "+str([player.name for player in self.team1])+", Team 2: "+str([player.name for player in self.team2  ])
                                 self.send(Message(text=self.teams), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
-                                picking = False
+                                self.picking = False
+                                self.capturn = -1
+                            elif(len(self.team2)<=len(self.team1)):
+                                pickmsg = self.team2[0].name+" make a pick"
+                                self.sendMessage(pickmsg, thread_id = self.gcuid, thread_type = ThreadType.GROUP)
+                                available = "Available: "+str(self.playernames-self.pickednames)
+                                self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
+                            else:
+                                self.capturn = 1
+                                pickmsg = self.team1[0].name+" make a pick"
+                                self.sendMessage(pickmsg, thread_id = self.gcuid, thread_type = ThreadType.GROUP)
+                                available = "Available: "+str(self.playernames-self.pickednames)
+                                self.send(Message(text=available), thread_id=self.gcuid, thread_type=ThreadType.GROUP)
                         else:
                             self.sendMessage("Invalid pick :|", thread_id = self.gcuid, thread_type = ThreadType.GROUP)
                     else:
-                        self.sendMessage("You're not a captain :'(", thread_id = self.gcuid, thread_type = ThreadType.GROUP)
+                        self.sendMessage("Not your turn :'(", thread_id = self.gcuid, thread_type = ThreadType.GROUP)
                 else:
                     self.sendMessage("Send !select_teams to start picking process", thread_id = self.gcuid, thread_type = ThreadType.GROUP)
             elif(message_object.text == '!start_game'):
-
+                playersrestructured = []
+                for i in range(len(self.team1)):
+                    playersrestructured.append(self.team1[i])
+                    playersrestructured.append(self.team2[i])
+                self.players = playersrestructured
                 if(self.teams==""):
                     random.shuffle(self.players)
                     self.teams = "Team 1: "+str([self.players[i].name for i in range(0,len(self.players),2)])+" | Team 2: "+str([self.players[i].name for i in range(1,len(self.players),2)])
